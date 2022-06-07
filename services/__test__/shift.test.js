4506653717744084dafcf09a9b217abd1ce74cdb
bishkou <chedyhammami@yahoo.com>
2022-06-07 10:19:36 +0200

    shifts tests done

--- services/__test__/shift.test.js
+++ services/__test__/shift.test.js
@@ -85,3 +85,129 @@ it('return a 400 for having 2 shifts in a day a shift to a worker', async () => 
         })
         .expect(400);
 });
+
+it('edits a shift', async () => {
+
+    const worker = await createWorker();
+    const shift = await createShift([true,false,false], '2021-06-06', worker._id, false, false);
+
+    const newShift = await request(app)
+        .post('/api/shift')
+        .send({
+            worker_id: worker._id,
+            work_date: shift.work_date,
+            day_shifts: shift.day_shifts,
+            is_holiday: shift.is_holiday,
+            is_weekend: shift.is_weekend
+        })
+        .expect(201);
+
+    await request(app)
+        .patch(`/api/shift/${newShift.body.id}`)
+        .send({
+            worker_id: worker._id,
+            day_shifts: [false, true, false]
+        })
+        .expect(200);
+
+    const response = await request(app)
+        .get(`/api/shift/${newShift.body.id}`)
+        .send({})
+        .expect(200);
+    expect(response.body.day_shifts).toEqual([false, true, false])
+
+});
+
+it('returns 400 if worker has more than one shift', async () => {
+
+    const worker = await createWorker();
+    const shift = await createShift([true,false,false], '2021-06-06', worker._id, false, false);
+
+    const newShift = await request(app)
+        .post('/api/shift')
+        .send({
+            worker_id: worker._id,
+            work_date: shift.work_date,
+            day_shifts: shift.day_shifts,
+            is_holiday: shift.is_holiday,
+            is_weekend: shift.is_weekend
+        })
+        .expect(201);
+
+    await request(app)
+        .patch(`/api/shift/${newShift.body.id}`)
+        .send({
+            worker_id: worker._id,
+            day_shifts: [true, true, false]
+        })
+        .expect(400);
+
+});
+
+it('returns no shift if is_holiday or is_weekend is true', async () => {
+
+    const worker = await createWorker();
+    const shift = await createShift([true,false,false], '2021-06-06', worker._id, false, false);
+
+    const newShift = await request(app)
+        .post('/api/shift')
+        .send({
+            worker_id: worker._id,
+            work_date: shift.work_date,
+            day_shifts: shift.day_shifts,
+            is_holiday: shift.is_holiday,
+            is_weekend: shift.is_weekend
+        })
+        .expect(201);
+
+    await request(app)
+        .patch(`/api/shift/${newShift.body.id}`)
+        .send({
+            worker_id: worker._id,
+            day_shifts: [false, true, false],
+            is_weekend: true
+        })
+        .expect(200);
+
+    const response = await request(app)
+        .get(`/api/shift/${newShift.body.id}`)
+        .send({})
+        .expect(200);
+    expect(response.body.day_shifts).toEqual([false, false, false])
+
+});
+
+it('deletes a shift from a worker', async () => {
+
+    const worker = await createWorker();
+    const shift = await createShift([true,false,false], '2021-06-06', worker._id, false, false);
+
+    const newShift = await request(app)
+        .post('/api/shift')
+        .send({
+            worker_id: worker._id,
+            work_date: shift.work_date,
+            day_shifts: shift.day_shifts,
+            is_holiday: shift.is_holiday,
+            is_weekend: shift.is_weekend
+        })
+        .expect(201);
+
+    await request(app)
+        .delete(`/api/shift/delete/${newShift.body.id}`)
+        .send({})
+        .expect(200);
+
+    const response = await request(app)
+        .get(`/api/shift/${newShift.body.id}`)
+        .send({})
+        .expect(200);
+    expect(response.body).toEqual({})
+
+    const response2 = await request(app)
+        .get(`/api/worker/${worker._id}`)
+        .send({})
+        .expect(200);
+    expect(response2.body.shifts).toEqual([])
+
+});
